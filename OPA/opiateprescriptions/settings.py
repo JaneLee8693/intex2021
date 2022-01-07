55043c753e54b739eb69665b879384f4ff1a3a79
YunChen Lee <80297424+JaneLee8693@users.noreply.github.com>
2022-01-06 17:19:48 -0700

    Update settings.py

--- OPA/opiateprescriptions/settings.py
+++ OPA/opiateprescriptions/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6_x)$7alu50jtqgej(dc%gr64pbo=ywu94cfs&j-)46r#ytz8_'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
